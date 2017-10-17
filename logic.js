7d8d708fa7285b39032b3aae1d1a82ad84449cce
EduardValentin <eduard.valentin96@yahoo.it>
2017-10-18 00:00:52 +0300

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,58 +1,99 @@
-// Global variables --------------------------------------------------------
 
-var iph8specH,
-    iph8priceH,
-    iphxspecH,
-    timer = null,
-    iphxpriceH;
 
-window.onload = function(){
 
+document.querySelector("body").style.marginTop = window.innerHeight + "px";
+
+window.onload = function() {
+    // Code here
+    
+    startSlideShow();
     createMainMenu();
-    mySlider();
+    mySlider();             // Elements Slider core
+
 };
 
 // Functions -------------------------------------------------------------
 
+function startSlideShow(){
+    var arr_slideDivs = document.getElementsByClassName("slide-img");
+    var i = 0;
+    
+    arr_slideDivs[0].style.opacity = 1;
+
+    timer = setInterval(function(){
+        if(i ==  arr_slideDivs.length-1) {
+            for(var j=0;j<arr_slideDivs;j++) {
+                arr_slideDivs[j].style.opacity = 0;     // Reset
+            }
+
+            i = 0;
+            arr_slideDivs[i].style.opacity = 1;
+        }
+        else {
+            i++;
+            arr_slideDivs[i].style.opacity = 1;
+        }
+    
+    }, 3500);
+}
+
 function mySlider() {
 
+    var arr_tables = document.getElementsByClassName("slide");
 
-    var arr_tables = document.getElementsByTagName("TABLE");
-
-    for(var i=0;i<arr_tables.length;i++){
-        if(arr_tables[i].id == "iph8-spec" ) {
-            iph8specH = arr_tables[i].offsetHeight;
+    for (var i = 0; i < arr_tables.length; i++) {
+        if (arr_tables[i].id == "iph8-spec") {
+            arr_tables[i].setAttribute("data-maxheight" ,  arr_tables[i].offsetHeight);
+        } else if (arr_tables[i].id == "iph8-price") {
+            arr_tables[i].setAttribute("data-maxheight" ,  arr_tables[i].offsetHeight);
+        } else if (arr_tables[i].id == "iphx-spec") {
+            arr_tables[i].setAttribute("data-maxheight" ,  arr_tables[i].offsetHeight);
+        } else if (arr_tables[i].id == "iphx-price") {
+            arr_tables[i].setAttribute("data-maxheight" ,  arr_tables[i].offsetHeight);
         }
-        else if(arr_tables[i].id == "iph8-price" ) {
-                iph8priceH = arr_tables[i].offsetHeight;
-
-             }
-            else if(arr_tables[i].id == "iphx-spec" ) {
-                    iphxspecH = arr_tables[i].offsetHeight;
-                 }
-                else if(arr_tables[i].id == "iphx-price" ) {
-                        iphxpriceH = arr_tables[i].offsetHeight;
-
-                    }
-
-
     }
 
-    var buttons = document.getElementsByClassName("slide-button");
+    // After we calculate the heights we set the closed class to tables
 
-    for(var i=0;i<buttons.length;i++){
-        buttons[i].addEventListener("click",buttonClicked());
+    var arr_slides = document.getElementsByClassName("slide");
+    for(let i =0;i<arr_slides.length;i++) {
+        arr_slides[i].className += " closed";
+        arr_slides[i].style.height = 0 + "px";
+        //arr_slides[i].style.visibility = "hidden";
     }
-}
 
-function buttonClicked() {
-    clearInterval(timer);
+    // Now we get all the slide buttons and add click events to them
 
+    var arr_slideButtons = document.getElementsByClassName("slide-button");
+    for(let i = 0; i<arr_slides.length;i++) {
+        arr_slideButtons[i].addEventListener("click", slideButtonClicked);
+    }
 
 }
 
-function createMainMenu()
-{
+function slideButtonClicked(event) {
+    var actionOnID = event.target.getAttribute("data-action");
+    var element = document.getElementById(actionOnID);
+    console.log(element);
+    if(element.classList.contains("closed")) {
+        // We open
+        element.classList.remove("closed");
+        element.classList.add("open");
+
+        element.style.height = element.getAttribute("data-maxheight") + "px";
+        //element.style.display = "block";
+
+        
+    } else {
+        // We close
+        element.classList.remove("open");
+        element.classList.add("closed");
+        element.style.height = 0 + "px";
+        //element.style.display = "none";
+    }
+}
+
+function createMainMenu() {
     var mainMenuHTML = `
         <ul>
             <li><a href="#"> Home </a></li>
@@ -61,16 +102,8 @@ function createMainMenu()
             <li><a href="#"> Iphone 8 Plus </a></li>
             <li><a href="#"> Iphone X </a></li>
         </ul>
-    `
+    `;
 
     var menu = document.getElementById("main-menu");
     menu.innerHTML = mainMenuHTML;
-}
-
-function getTables(children){
-    var arr = new Array();
-    for(;children;children=children.nextSibbling)
-        if(children.tagName == "TABLE")
-            arr.push(children);
-    return arr;
 }
